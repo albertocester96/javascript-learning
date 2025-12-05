55fbbf69d3d66e62875c9abfc3841e22e68078ce
albertocester96 <albertocester96@gmail.com>
2025-12-05 16:45:14 +0100

    esercizio 2.1 - to.do-list.+ stato creato lo sceheltro della to do list, in cui si può agigugnere delle attività che vengono salvate su localStorage e mostrate

---
+++ sezione2/script2.js
@@ -0,0 +1,49 @@
+
+class Task {
+    constructor(title, description, status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+}
+
+const addTaskButton = document.querySelector("#addTaskButton");
+const taskTitle = document.querySelector(".js-title");
+const taskDescription = document.querySelector(".js-description");
+
+const taskListContainer = document.querySelector(".js-taskList");
+
+
+let tasks = []; 
+
+function displayTasks() {
+
+    taskListContainer.innerHTML = "";
+
+    for (let i=0; i<localStorage.length; i++){
+        const task = JSON.parse(localStorage.getItem(localStorage.key(i)));
+
+        console.log(task);
+
+        tasks.forEach(task => {
+            const taskElement = document.createElement("div");
+            taskElement.innerHTML = `<h3>${task.title}</h3>
+                                     <p>${task.description}</p>
+                                     <p>Status: ${task.status}</p>
+                                     <hr>`;
+            taskListContainer.appendChild(taskElement);
+        });
+       
+    }
+}
+
+addTaskButton.addEventListener("click", function() {
+    newTask = new Task(taskTitle.value, taskDescription.value, "incomplete");
+    tasks.push(newTask);
+    localStorage.setItem("task", JSON.stringify(tasks));
+
+    displayTasks();
+});
+
+
+
